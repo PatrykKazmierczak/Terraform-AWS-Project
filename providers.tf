809f5ec49c46d7c27a8433ae7899bbc4d8b162c1
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2024-09-13 10:11:02 +0200

    update

--- providers.tf
+++ providers.tf
@@ -1,26 +1,26 @@
-
 # Specify the minimum version of Terraform required for this configuration
 terraform {
-  required_version = "1.9.5"
-}
+  required_version = "1.9.5"  # Terraform version 1.9.5 or higher is required
 
-# Define the required providers and their versions
-required_providers {
-  aws = {
-    source  = "hashicorp/aws"  # Provider source from HashiCorp
-    version = "5.66.0"         # Exact version of the AWS provider required
+  # Define the required providers and their versions
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"  # Provider source from HashiCorp
+      version = "5.66.0"         # Exact version of the AWS provider required
+    }
   }
-}
 
-# Configure the backend to store the Terraform state file
-backend "s3" {
-  bucket         = "terraform-videopoint-state-backup"  # S3 bucket name where the state file will be stored
-  key            = "terraform.tfstate"                  # Path within the S3 bucket for the state file
-  region         = "eu-north-1"                         # AWS region where the S3 bucket is located
-  dynamodb_table = "terraform-videopoint"                # DynamoDB table for state locking and consistency checking
+  # Configure the backend to store the Terraform state file
+  backend "s3" {
+    bucket         = "terraform-videopoint-state-backup"  # S3 bucket name where the state file will be stored
+    key            = "terraform.tfstate"                  # Path within the S3 bucket for the state file
+    region         = "eu-north-1"                         # AWS region where the S3 bucket is located
+    dynamodb_table = "terraform-videopoint"                # DynamoDB table for state locking and consistency checking
+  }
 }
 
 # Configure the AWS provider
 provider "aws" {
   region = "eu-north-1"  # AWS region where resources will be created
 }
+
